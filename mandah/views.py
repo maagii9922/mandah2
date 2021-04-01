7ddab6e93dde1dbaf040980a7ec394d6b32cc105
m <m@gmail.com>
2021-04-01 17:54:38 +0800

    sss

--- mandah/views.py
+++ mandah/views.py
@@ -17,24 +17,21 @@ def login(request):
     return render(request,'login.html')
 
 def shalgah(request):
+    # h=Hereglegch.objects.all()
+    # return render(request,'home.html',{'hereglegch':h})
     print(request.method)
     x=request.POST['name']
     y=request.POST['code']
+    # print(y)
 
     h=Hereglegch.objects.filter(name=x,code=y)
-    
-    return render(request,'login.html',{'aldaa':'aldaa garlaa','buruu':x,'buruu2':y})
-
-
-
-    # print(len(h))
-    # if len(h)==1: 
-    #     # return HttpResponse('OK')
-    #     b=Baraa.objects.all()
-    #     return render(request,'home.html',{'user':h[0].name, 'baraa':b})
-    # elif len(h)==0:
-    #     # return HttpResponse('error')
-    #     return render(request,'login.html',{'aldaa':'aldaa garlaa','buruu':x,'buruu2':y})
-
         
-    # return HttpResponse('server error')
+
+    print(len(h))
+    if len(h)==1: 
+        return render(request,'home.html',{'user':h[0].name})
+    elif len(h)==0:
+        # return HttpResponse('error')
+        return render(request,'login.html',{'aldaa':'aldaa garlaa','buruu':x,'buruu2':y})
+        
+    return HttpResponse('server error')
